75abb930cc91d5d4082316ac49130a7618697886
mazout <nicolas.gerelm@gmail.com>
2016-10-30 22:54:25 -0400

    First push for development

---
+++ Controlleur/DataControlleur.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections.Generic;
+using Vulpix;
+public class DataControlleur
+    {
+        public void GetData(Req req, Res res){
+            var fooList = new List<String>();
+            fooList.Add("Hello");
+            fooList.Add("World");
+            
+            res.Send(fooList);
+        }
+    }
---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Vulpix;
+using Microsoft.AspNetCore.Http;
+namespace ConsoleApplication
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var app = new VulpixServer();
+            
+            app.UseStaticFolder("public");
+
+            //entry point for polymer
+            app.AddRoute("GET","/", (Req req, Res res)=>{
+                 res.Response.SendFileAsync("public/index.html");
+            });
+
+            var DataControlleur = new DataControlleur();
+            app.AddRoute("GET","/Data",DataControlleur.GetData);   
+
+            app.Listen(3000);
+        }
+    }
+}
